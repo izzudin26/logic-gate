2943ded3c948cc59614823c41c3939b295b52602
Azidarmor26 <azidarmor26@gmail.com>
2020-03-14 02:23:07 +0700

    just write js

---
+++ main.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector("#button")
+let val1 = document.querySelector("#val1")
+let val2 = document.querySelector("#val2")
+btn.addEventListener("click", () => {
+    console.log(val1.value)
+})
